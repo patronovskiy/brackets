f955e87aa206def489c0eba3405bb3ccc6743590
Galina Martiushova (patronovskiy) <patronovskiy@mail.ru>
2019-03-08 10:28:54 +0500

    feat: add fuction for checking closing brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	str = str.split('');
+	let checkTimes = str.length/2 + 1;
+
+	for (let i = 0; i <= checkTimes; i ++) {
+		 str.forEach(function(item, i, arr) {
+		 	for (let n = 0; n < bracketsConfig.length; n ++) {
+		 		if (arr[i] === bracketsConfig[n][0] && arr[i+1] === bracketsConfig[n][1]) {
+		 			str.splice(i, 2);
+		 		};
+		 	};
+		 });
+
+		if (str.length === 0) break;
+	};
+		
+	if (str.length === 0) {
+		return true;
+	} 
+	else {
+		return false;
+	}
+};
